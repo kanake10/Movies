4663c6e8b4e46ac0fab93541dc25c1147f32c47a
kanake10 <10kanake@gmail.com>
2021-12-21 14:56:55 +0300

    network

--- app/src/main/java/com/example/movies/network/MovieApi.kt
+++ app/src/main/java/com/example/movies/network/MovieApi.kt
@@ -1,15 +1,14 @@
 package com.example.movies.network
 
 import com.example.movies.model.GetMoviesResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface Api {
-
+interface MovieApi {
     @GET("movie/popular")
     fun getPopularMovies(
-        @Query("api_key") apiKey: String = "c6b60cf1108ccfe7a3d10b96525dff17",
+        @Query("api_key") apiKey: String = "api_key",
         @Query("page") page: Int
-    ): Call<GetMoviesResponse>
+    ): Response<GetMoviesResponse>
 }
