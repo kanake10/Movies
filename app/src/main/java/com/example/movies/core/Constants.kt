eadaaedf323f9c9b51e1d9230230371f4ab7d2c0
kanake10 <10kanake@gmail.com>
2021-12-22 23:01:00 +0300

    util

---
+++ app/src/main/java/com/example/movies/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.movies.core
+
+class Constants {
+
+    companion object {
+        const val api_key = "c6b60cf1108ccfe7a3d10b96525dff17"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
+}
