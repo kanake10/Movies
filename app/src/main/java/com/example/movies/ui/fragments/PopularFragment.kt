3f0f80c9d79125778d548e12451fbec2ec0694a3
kanake10 <10kanake@gmail.com>
2022-02-11 14:06:15 +0300

    ui

--- app/src/main/java/com/example/movies/ui/fragments/PopularFragment.kt
+++ app/src/main/java/com/example/movies/ui/fragments/PopularFragment.kt
@@ -1,7 +1,86 @@
 package com.example.movies.ui.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies.R
+import com.example.movies.adapter.MovieAdapter
+import com.example.movies.core.Resource
+import com.example.movies.databinding.FragmentPopularBinding
+import com.example.movies.ui.MoviesActivity
+import com.example.movies.ui.viewmodel.MoviesViewModel
 
-class PopularFragment : Fragment(R.layout.fragment_popular){
+class PopularFragment : Fragment() {
+
+    private var _binding: FragmentPopularBinding? = null
+    private val binding get() = _binding!!
+    lateinit var viewModel : MoviesViewModel
+    lateinit var movieAdapter: MovieAdapter
+
+    val TAG = "PopularMovies"
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPopularBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MoviesActivity).viewModel
+        setupRecyclerView()
+
+
+        viewModel.popularMovies.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { movieResponse ->
+                        movieAdapter.differ.submitList(movieResponse.results)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.rvPopularMovies.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
